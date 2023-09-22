9bd82add05b8cc3afe9ee4f7028c77b0cd83c582
Aurelien <aurelien.fillion.pro@gmail.com>
2023-09-22 08:20:17 +0200

    refactor: suppression de findbyuuid

--- src/api/wine-bottle/controllers/wine-bottle.js
+++ src/api/wine-bottle/controllers/wine-bottle.js
@@ -11,32 +11,9 @@ module.exports = createCoreController('api::wine-bottle.wine-bottle', ({ strapi 
   lifecycles:{
       async beforeCreate(data) {
     
-          data.qrCodeUrl ="wine-bottle\\" + crypto.randomUUID();
+          data.url ="wine-bottle\\" + crypto.randomUUID();
 
         },  
   },
-     
-
-  async findByUuid(ctx) {
-      
-      const uidBottle  = ctx.params.uid;
-
-      try {
-          const wineBottle = await strapi.db.query('api::wine-bottle.wine-bottle').findOne({
-              select: ['name','uid','qrCodeUrl'],
-              where: { id: uidBottle },
-              populate: { category: true },
-            });
-                
-        if (!wineBottle) {
-          return ctx.notFound('Wine bottle not found');
-        }
-  
-        ctx.send(wineBottle);
-
-      } catch (error) {
-        ctx.throw(500, 'Internal Server Error');
-      }
-  },   
 
 }));
