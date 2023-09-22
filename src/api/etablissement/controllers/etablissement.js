a16338914e483182f44b48f1a21111cf4b083ae3
Lucas Goï <lucasgoi746@gmail.com>
2023-09-22 08:44:23 +0200

    build: ajout des routes pour récupérer les médias

--- src/api/etablissement/controllers/etablissement.js
+++ src/api/etablissement/controllers/etablissement.js
@@ -6,4 +6,35 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::etablissement.etablissement');
+module.exports = createCoreController('api::etablissement.etablissement', ({ strapi }) => ({
+    async getEtablissementMedias(ctx) {
+        try{
+            const id = ctx.params.id;
+
+            const etablissement = await strapi.entityService.findOne('api::etablissement.etablissement', id, {
+                populate: { category: true },
+              });
+
+            if(etablissement){
+                try {
+                    const result = await strapi.entityService.findOne('api::etablissement.etablissement', id, {
+                      filters: {
+                        id: id
+                      },
+                      populate: '*',
+                    })
+                    ctx.body = result;
+                  } catch (err) {
+                    console.error("Erreur lors de l'exécution de la requête SQL :", err);
+                    ctx.status = 500;
+                    ctx.body = { error: "Une erreur s'est produite lors de la récupération de l'image." };
+                  }
+                  
+            }else{
+                ctx.notFound()
+            }
+        } catch(err){
+            ctx.body = err
+        }
+    }
+}));
--- src/api/wine-bottle/controllers/wine-bottle.js
+++ src/api/wine-bottle/controllers/wine-bottle.js
@@ -6,4 +6,35 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::wine-bottle.wine-bottle');
+module.exports = createCoreController('api::wine-bottle.wine-bottle', ({ strapi }) => ({
+     async getWineBottleMedias(ctx) {
+        try{
+            const id = ctx.params.id;
+
+            const wineBottle = await strapi.entityService.findOne('api::wine-bottle.wine-bottle', id, {
+                populate: { category: true },
+              });
+
+            if(wineBottle){
+                try {
+                    const result = await strapi.entityService.findOne('api::wine-bottle.wine-bottle', id, {
+                      filters: {
+                        id: id
+                      },
+                      populate: '*',
+                    })
+                    ctx.body = result;
+                  } catch (err) {
+                    console.error("Erreur lors de l'exécution de la requête SQL :", err);
+                    ctx.status = 500;
+                    ctx.body = { error: "Une erreur s'est produite lors de la récupération de l'image." };
+                  }
+                  
+            }else{
+                ctx.notFound()
+            }
+        } catch(err){
+            ctx.body = err
+        }
+    }
+}));
